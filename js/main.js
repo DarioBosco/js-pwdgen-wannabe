711f98f073b12b2fe3b9833573552d44a7dd91da
Dario Bosco <dario_bosco@protonmail.com>
2021-07-14 12:55:48 +0200

    Aggiornato nomi variabili

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // Chiedo all'utente il suo nome e lo tengo come variabile
-var name = prompt('Inserisci il tuo nome');
+var userName = prompt('Inserisci il tuo nome');
 
 // Chiedo all'utente il suo cognome e lo tengo come variabile
-var surname = prompt('Inserisci il tuo cognome');
+var userSurname = prompt('Inserisci il tuo cognome');
 
 // Chiedo all'utente il suo colore preferito e lo tengo come variabile
-var favColor = prompt('Inserisci il tuo colore preferito');
+var UserFavColor = prompt('Inserisci il tuo colore preferito');
 
-document.getElementById('pwdResult').innerHTML = 'Password generata: ' + name + surname + favColor + '21';
+document.getElementById('pwdResult').innerHTML = 'Password generata: ' + userName + userSurname + UserFavColor + '21';
